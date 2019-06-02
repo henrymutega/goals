41af5dc45dce86157ccd5d53182daf055739c892
henrymutega <hmutega@gmail.com>
2019-06-02 15:40:55 +0300

    Add changes to index.html

--- triangle_project/js/scripts.js
+++ triangle_project/js/scripts.js
@@ -1,16 +1,19 @@
-var sideA = parseInt(prompt("Enter the first side"));
-var sideB = parseInt(prompt("Enter the second side"));
-var sideC = parseInt(prompt("Enter the third side"));
+var result = function(){
+  var sideA = document.getElementById('sideA').value;
+  var sideB = document.getElementById('sideB').value;
+  var sideC = document.getElementById('sideC').value;
 
-if(sideA===sideB && sideA===sideC && sideB===sideC){
-  alert("This is an Equilateral Triangle.");
-}
-else if(sideA===sideB || sideA===sideC || sideB===sideC){
-  alert("This is an Isosceles Triangle.");
-}
-else if(sideA!==sideB && sideA!==sideC && sideB!==sideC){
-  alert("This is Scalene Triangle.");
-}
-else{
-  alert("This is not a Triangle!")
-}
+  var output = document.getElementById('result');
+
+   if(sideA<=0 || sideB<=0 || sideC<=0){
+    document.getElementById('result').value = output + "Invalid inputs.";
+  }else if(sideA!==sideB && sideA!==sideC && sideB!==sideC) (sideA+sideB>sideC && sideA+sideC>sideB && sideB+sideC>sideA){
+    document.getElementById('result').value = output + "This is Scalene Triangle.";
+  }else if(sideA===sideB && sideA===sideC && sideB===sideC){
+    document.getElementById('result').value = output + "This is an Equilateral Triangle.";
+  }else if(sideA===sideB || sideA===sideC || sideB===sideC){
+    document.getElementById('result').value = output + "This is an Isosceles Triangle.";
+  }else if(sideA+sideB<=sideC && sideA+sideC<=sideB && sideB+sideC<=sideA){
+      document.getElementById('result').value = output + "This is not a Triangle!";
+  }
+};
